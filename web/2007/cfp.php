6ed0b72fa652767afd3237b9eb8a7e3df54dea77
ford <brynosaurus@gmail.com>
2010-08-02 10:00:17 -0400

    Incorporate archive of PFLDnet 2007 web site snapshot from Lachlan Andrew

---
+++ web/2007/cfp.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<HTML lang="en">
+
+<HEAD>
+<META http-equiv="content-type" content="text/html;charset=ISO-8859-1">
+<TITLE>PFLDnet 2007- Call for papers</TITLE>
+<LINK href="def.css" rel="stylesheet" media="all">
+</HEAD>
+
+<?php $ROOT = "\\\\wil-fs\\wwwroot\\pfldnet2007"; ?>
+<?php include ($ROOT."\\templates\\header.php"); ?>
+
+<TABLE width="780" border="0" cellspacing="0" cellpadding="0">
+<TR>
+<TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="5" width="1" border="0"></TD>
+<TD width="778"><H1><IMG src="images/cap_cfp.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">Call for Papers </H1>
+<div class="top">
+<div style="text-align:center"><!-- p style="font-size:16px;color:#cc0000">Abstract Submission now closed. </p -->
+<!--a href="https://regist.e-side.co.jp/PFLDnet2006/abstract.php" target="_blank"><img src="images/submit_button.PNG" alt="SUBMIT ABSTRACT" width="140" height="71" border="0"></a></div -->
+<a href="submission.php"><img src="images/submit_button.PNG" alt="SUBMIT ABSTRACT" width="140" height="71" border="0"></a></div>
+<!--p style="text-align:center;font-size:14px;color:#CC0000;font-weight:bold">Abstract Submission now closed. </p -->
+
+<p>Future networks will increasingly contain high bandwidth, high delay paths.  Protocols for fast, long distance networks will have to expand from experimental testbeds and into the wider world. In that process, it is increasingly important for protocols which achieve high utilization on fast, long distance networks also to have
+the qualities required for widespread use, such as fairness and scalability to many users.</p>
+<p>PFLDnet2007 solicits papers which will further research towards on protocols  for tomorrow's high speed internet, at all stages along the continuum from the requirements of today's grid networks, through benchmarking of proposals, to the final selection of the next generation of TCP.</p>
+<p>Participants wishing to present a paper should upload a full paper to the submission site by <span  style="text-decoration: line-through">Friday 13 October, 2006 Friday 20 October , 2006 (Extended)</span>.<br>
+Papers should be typically 4-5 pages, but no longer than 6 pages.</p>
+<dl>
+<dt>Scope:<SPAN class="medium"> (choose at least one of the subject areas below - multiple selections possible)</SPAN></dt>
+<ul>
+  <li>Protocol issues in fast long-distance networks</li>
+  <li>Protocol development
+    <ul>
+      <li>Enhancements of TCP and its variants</li>
+      <li>Novel data transport protocols designed for new application services</li>
+      <li>Explicit signaling protocols: Optimization criteria and deployment strategies</li>
+      <li>Pacing and shaping of TCP and UDP traffic</li>
+      <li>Parallel transfers and multistreaming</li>
+      </ul>
+  </li>
+  <li>Performance evaluation
+      <ul>
+        <li>Modeling and simulation-based results</li>
+        <li>Experiments on real networks and actual measurements</li>
+        <li>Protocol benchmarking</li>
+      </ul>
+  </li>
+  <li>Hardware-specific issues </li>
+  <ul>
+    <li>Transport over optical networks</li>
+    <li>RDMA over WANs</li>
+    <li>Protocol implementation and hardware issues (PCs, NICs, TOEs, routers, switches, etc.)</li>
+    <li>Data replications and striping</li>
+  </ul>
+  <li>Application focus 
+    <ul>
+        <li>Requirements and experience from bandwidth demanding applications</li>
+        <li>Bulk-data transfer applications both TCP and non-TCP based</li>
+        <li>Transport service for Grids</li>
+        <li>QoS and scalability issues</li>
+        <li>Multicast over fast long-distance networks</li>
+        </ul>
+  </li>
+  </ul>
+</dl>
+
+<p>Authors whose papers are selected for presentation will have the option to submit a revised paper,
+to be published on the PFLDnet 2007 web site and in the PFLDnet 2007 proceedings.</p>
+<dl>
+<dt>Important Dates</dt>
+<dd><strong>Initial paper submission deadline:</strong> <span  style="text-decoration: line-through">Friday 13 October, 2006Friday 20 October , 2006 (Extended)</span><br>
+Acceptance notification:<span  style="text-decoration: line-through"> Monday 4 December, 2006</span><br>
+Final paper submission:<span  style="text-decoration: line-through"> Monday 22 January, 2007</span><br>
+Workshop: Wednesday and Thursday, 7 and 8 February, 2007<br>
+Tutorial: (Optional enrolment) Friday, 9 February, 2007, 10:00-11:15 <br>
+Site visit: (Free) WAN-in-Lab site visit at Caltech Friday, 9 February, 2007, 14:00-16:00
+</dd>
+</dl>
+</div>
+
+<?php include ($ROOT."\\templates\\footer.php"); ?>
---
+++ web/2007/submission.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<HTML lang="en">
+
+<HEAD>
+<META http-equiv="content-type" content="text/html;charset=ISO-8859-1">
+<TITLE>PFLDnet 2007 - Paper submission instructions</TITLE>
+<LINK href="def.css" rel="stylesheet" media="all">
+</HEAD>
+
+<?php $ROOT = "\\\\wil-fs\\wwwroot\\pfldnet2007"; ?>
+<?php include ("\\\\wil-fs\\wwwroot\\templates\\functions.html"); ?>
+<?php include ($ROOT."\\templates\\header.php"); ?>
+
+<TABLE width="780" border="0" cellspacing="0" cellpadding="0">
+<TR>
+<TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="5" width="1" border="0"></TD>
+<TD width="778"><H1><IMG src="images/cap_sub.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">SUBMISSION INSTRUCTIONS</H1>
+  <!-- <h2>Papers are to be electronically submitted to EDAS at <A href="http://www.edas.info/" target=_blank><strong>http://www.edas.info/</strong></A> </h2> -->
+  <P><B><font color="red">Important Date</font></B>: Final Paper Submission <span style= "text-decoration: line-through">Wednesday 22 January, 2007</span>
+  <!-- The paper submission deadline has been extended from <span style="text-decoration: line-through">Friday 13 October, 2006
+      to 12 midnight EST, Friday 20 October , 2006 (Extended)</span>.--> </P>
+  <p><B><font color="red">PFLDnet 2007 Paper Format:</font></B></P>
+<p>The papers are to submitted using <B>font size 10pt.</B><br>
+Papers should be about <B>4-5&nbsp;printed pages</b> in length, <b>including figures</B>, up to maximum of <strong>6&nbsp;pages</strong>. <br>
+The papers are to follow the format of the <a href="http://www.ieee.org/portal/site/mainsite/menuitem.818c0c39e85ef176fb2275875bac26c8/index.jsp?&pName=corp_level1&path=pubs/transactions&file=stylesheets.xml&xsl=generic.xsl" target="_blank">IEEE Transaction style</a> for LaTeX and Word style files. Use the <STRONG>
+      <a href="http://www.ieee.org/organizations/pubs/confpub/auxfiles/sample_manuscript.pdf">sample manuscript</a></STRONG> as a visual aid for formatting and LaTeX example can be also found <a href="IEEE_CS_Latex.zip">here</a>.</P>
+<p><b><font color="red">Camera Ready Format</font></b>: The camera ready format will be the same as the submission format, i.e., 4-5 or at most 6 pages long with a font size of 10pt. More information will be added after the paper acceptance date. </P>
+<p><b><font color="red">Paper Submission</font></b>: </p>
+<p>See the <a href="authors.php">AUTHOR KIT</a> before submitting any papers to PFLDnet2007. </p>
+<!--
+<p>Authors should submit their papers through the EDAS web site <A href="http://www.edas.info/" target=_blank><strong>http://www.edas.info/</strong></A> in three steps: </p>
+<ol>
+<li>Creation of a personal account on EDAS (<B>if the author does not already have one)</B><br>
+(After the author completes log-in at http://www.edas.info/ follow the links to http://www.edas.info/listConferencesSubmit.php and scroll down the &quot;conference&quot; column on the webpage to PFLDnet2007, then follow the prompts to submit your paper.) </li>
+<li>Registration of the paper (requiring a short abstract of up to 150 words)</li>
+<li>Upload of the paper. <B>Only in pdf format</B></li>
+</ol>
+For help with EDAS please contact the <php email_link("edas-help AT edas info", "EDAS Administrator");>.
+</p>
+-->
+<!--
+<p><b><font color="red">Rules</font></b>: Only original papers written in English that have not been published or submitted for publication elsewhere can be submitted.
+Full papers are required for review.
+All paper submissions will be carefully reviewed by at least two experts.
+Reviews will be returned to the author(s) with comments to ensure the high quality of the accepted papers.
+-->
+<p>The authors of accepted papers must guarantee that their paper will be presented at the conference.
+At least one author of each accepted paper must be registered for the conference in order for that paper to appear in the proceedings and to be scheduled for presentation.
+</p>
+</TD>
+</TR>
+</TABLE>
+
+
+<?php include ($ROOT."\\templates\\footer.php"); ?>
