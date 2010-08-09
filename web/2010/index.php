01559cf7a0ab4a040796947722414779e4541cb5
Jana Iyengar <jiyengar@surutti.fandm.edu>
2010-08-09 15:53:34 -0400

    added 2010

---
+++ web/2010/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<HTML lang="en">
+
+<HEAD>
+<META http-equiv="content-type" content="text/html;charset=ISO-8859-1">
+<TITLE>PFLDnet 2010</TITLE>
+<LINK href="def.css" rel="stylesheet" media="all">
+</HEAD>
+
+<?php $ROOT = "."; ?>
+<?php $page="front"; include ($ROOT."/templates/header.php"); ?>
+<?php include($ROOT."/templates/functions.html"); ?>
+
+<TABLE width="778" border="0" cellspacing="8" cellpadding="0">
+<TR valign="top">
+<TD class="medium" width="524">
+				
+<p>The 8th International Workshop on Protocols for Future, Large-Scale &amp; 
+Diverse Network Transports (PFLDNeT) brings together researchers and 
+practitioners from all over the world
+working in the area of transport layer issues for 
+modern communication networks.
+
+ <p class="top">Previous PFLDNets held at 
+CERN 
+(<a href="http://datatag.web.cern.ch/datatag/pfldnet2003/" target="_blank">PFLDnet2003</a>),
+  Argonne 
+(<a href="http://www-didc.lbl.gov/PFLDnet2004/" target="_blank">PFLDnet2004</a>),
+  Lyon 
+	(<a href="http://www.ens-lyon.fr/LIP/RESO/pfldnet2005/" target="_blank">PFLDnet2005</a>),
+  Nara 
+	(<a href="http://www.hpcc.jp/pfldnet2006/" target="_blank">PFLDnet2006</a>),
+ISI
+	(<a href="http://wil.cs.caltech.edu/pfldnet2007/">PFLDnet2007</a>),
+Manchester (PFLDnet2008),
+and Tokyo (<a href="http://www.hpcc.jp/pfldnet2009/Top.html">PFLDNeT2009</a>)
+  were very successful in bringing together researchers from all over the
+	world, including North America, Europe and Asia.
+This workshop will continue in this tradition of
+providing researchers a focussed and highly interactive environment
+to exchange their ideas and 
+experiences in the area of transport layer problems.</p>
+
+</TD>
+<TD class="medium" rowspan="2" width="3" background="images/line_h.gif"><IMG src="images/spacer.gif" alt="" height="5" width="3" border="0"></TD>
+<TD class="medium" rowspan="2" align="center">
+<a href="submission.php"><img src="images/submit_button.PNG" alt="SUBMIT ABSTRACT" width="140" height="71" border="0"></a><br/>
+<IMG src="images/cap_other.gif" alt="other contents" height="18" width="197" border="0" vspace="5"><BR>
+<A href="http://www.fandm.edu" target="_blank"><IMG src="images/bt_venue.gif" alt="VENUE" height="33" width="178" border="0" vspace="3"></A><BR>
+<!-- <A href="aboutlancaster.php"><IMG src="images/bt_LA.gif" alt="about Lancaster" height="33" width="178" border="0" vspace="3"></A><BR> 
+<A href="regist.php"><IMG src="images/bt_regist.gif" alt="registration" height="33" width="178" border="0" vspace="3"></A><BR>
+<A href="accommodation.php"><IMG src="images/bt_accom.gif" alt="accommodation" height="33" width="178" border="0" vspace="3"></A><BR>
+<A href="access.php"><IMG src="images/bt_access.gif" alt="access" height="33" width="178" border="0" vspace="3"></A><BR>
+<A href="visas.php"><IMG src="images/bt_visas.gif" alt="visas" height="33" width="178" border="0" vspace="3"></A><BR> -->
+<!--
+<IMG src="images/cap_contact.gif" alt="contact local organizers" height="19" width="197" border="0" vspace="3"><BR><img src="images/mail.gif" width="135" height="15" alt="pfldnet2006-la [at] hpcc.jp"><BR>
+-->
+<IMG src="images/cap_contact.gif" alt="contact local organizers" height="19" width="197" border="0" vspace="3"><BR>
+	jiyengar/fandm/edu <br>(fill in appropriately)
+<BR>
+<BR>
+</TD>
+</TR>
+<TR>
+<TD width="524" valign="top" class="medium"><IMG src="images/inportantday.gif" alt="important dates" height="29" width="524" border="0">
+<p></p>
+
+<DIV class="top">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin-left:20px" class="detail">
+  <tr>
+    <td valign="top"><strong>Abstract submission:</strong></td>
+    <td><span  style="text-decoration: none"><strong>September 20, 2010</strong><br/></span></td>
+  </tr>
+  <tr>
+    <td valign="top"><strong>Paper submission:</strong></td>
+    <td><span  style="text-decoration: none"><strong>September 27, 2010</strong><br/></span></td>
+  </tr>
+  <tr>
+    <td>Notification of acceptance:</td>
+    <td><span  style="text-decoration: none">October 25, 2010</span></td>
+  </tr>
+  <tr>
+    <td>Camera-ready submission:</td>
+    <td><span  style="text-decoration: none">November 14, 2010</span></td>
+  </tr>
+  <tr>
+    <td>Registration:</td>
+    <td><span  style="text-decoration: none">TBD</span></td>
+  </tr>
+  <tr>
+    <td valign="top">Workshop:</td>
+    <td>November 28-29, 2010<br></td>
+  </tr>
+  <tr>
+	   <td valign="top"><p><strong>IRTF ICCRG Meeting (co-located):</strong></p>
+      </td>
+    <td>November 29, 2010<br>	</td>
+  </tr>
+ <tr>
+	 <td valign="top" colspan=2><br><p>Note: 
+	 PFLDNeT 2010 will be immediately before 
+	 <a href="http://conferences.sigcomm.org/co-next/2010/">
+	 ACM CoNEXT 2010</a>, and its venue
+	 is located only a short drive or train ride from Philadelphia, making it
+	 easy to attend both events in one trip.</p>
+      </td>
+ </tr>
+</table>
+</DIV>
+</TD>
+</TR>
+</TABLE>
+
+<?php include ($ROOT."/templates/footer.php"); ?>
---
+++ web/2010/templates/footer.php
@@ -0,0 +1,41 @@
+<HR class="dotline">
+</TD>
+<TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="5" width="1" border="0"></TD>
+</TR>
+</TABLE>
+<TABLE width="780" border="0" cellspacing="0" cellpadding="0">
+<TR>
+<TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="24" width="1" border="0"></TD>
+<TD class="small" align="right" colspan="2"><A href="./">HOME</A> | <A href="cfp.php">CFP</A> <!-- | <A href="submission.php">SUBMISSION INSTRUCTIONS</A> -->| <A href="technical.php">TECHNICAL PROGRAM</A> | <A href="committees.php">COMMITTEES</A> | <a href="authors.php">AUTHOR KIT</a>&nbsp;&nbsp;<BR>
+<A href="aboutlancaster.php">ABOUT LANCASTER</A> | <A href="regist.php">REGISTRATION</A> | <A href="accommodation.php">ACCOMMODATION</A> | <A href="access.php">ACCESS</A> | <A href="visas.php">VISAS</A>&nbsp;&nbsp;</TD>
+<TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="24" width="1" border="0"></TD>
+</TR>
+<TR>
+<TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="24" width="1" border="0"></TD>
+<td class="small" bgcolor="#477cc8"><!-- CQ Counter code start -->
+<script type="text/javascript" language="javascript"><!-- 
+_d=document; _n=navigator; _t=new Date(); function t() { _d.write( 
+"<img src=\"http://us.2.cqcounter.com/cgi-bin/c?_id=PFDL07&_z=0&_r="+
+_r+"&_c="+_c+"&_j="+_j+"&_t="+(_t.getTimezoneOffset())+"&_k="+_k+
+"&_l="+escape(_d.referrer)+"\" width=70 height=15 "+
+"border=0>");} _c="0"; _r="0"; _j="U"; _k="U"; _d.cookie="_c=y";
+_d.cookie.length>0?_k="Y":_k="N";//--></script>
+<script type="text/javascript" language="javascript1.2"><!-- 
+_n.javaEnabled()?_j="Y":_j="N";_b=screen; _r=_b.width; 
+_n.appName!="Netscape"?_c=_b.colorDepth : _c=_b.pixelDepth;//--></script>
+<a title="Free Hit Counter" href="http://cqcounter.com/?_id=PFDL07&_lo=us2" 
+target="_top"><script type="text/javascript" language="javascript"><!-- 
+t(); //--></script></a><noscript><img width="70" height="15" border="0" 
+alt="Counter" src="http://us.2.cqcounter.com/cgi-bin/c?_id=PFDL07&_z=0"><br>
+<a title="Web Page Counter" href="http://cqcounter.com/">Website Statistics</a></noscript>
+<!-- CQ Counter code end -->
+Page hosted on <a href="http://wil.cs.caltech.edu/"><font color="#000000">WAN-in-Lab</font></a>
+</td><TD class="crtex" align="right" bgcolor="#477cc8" width="500">PFLDnet 2007&nbsp;&nbsp;</TD>
+<TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="24" width="1" border="0"></TD>
+</TR>
+</TABLE>
+<BR>
+</DIV>
+</BODY>
+
+</HTML>
