a27c7f5656c2504dc5d099120034180803c27bd6
ford <brynosaurus@gmail.com>
2010-09-07 17:03:12 -0400

    sync up

--- web/2010/submission.php
+++ web/2010/submission.php
@@ -14,7 +14,8 @@
 <TABLE width="780" border="0" cellspacing="0" cellpadding="0">
 <TR>
 <TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="5" width="1" border="0"></TD>
-<TD width="778"><H1><IMG src="images/cap_sub.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">SUBMISSION INSTRUCTIONS</H1>
+<TD width="778"><H1><IMG src="images/cap_sub.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">Submission Instructions</H1>
+<div class="top">
 
 <H2> Paper Format:</H2> 
 
@@ -41,40 +42,10 @@ Papers must be submitted via EDAS following these instructions:<br>
 4. Register the paper (requires paper title and abstract)<br>
 5. Once the paper is registered, you can add authors and
 upload manuscript in PDF format.
-
-The authors of accepted papers must guarantee that their paper will be presented at the conference. At least one author of each accepted paper must be registered for the conference in order for that paper to appear in the proceedings and to be scheduled for presentation. 
-
-  <!-- <h2>Papers are to be electronically submitted to EDAS at <A href="http://www.edas.info/" target=_blank><strong>http://www.edas.info/</strong></A> </h2> -->
-  <p><B><font color="red">PFLDnet 2010 Paper Format:</font></B></P>
-<p>The papers are to submitted using <B>font size 10pt.</B><br>
-Papers should be a maximum of <strong>6&nbsp;pages</strong> in length. <br>
-The papers are to follow the format of the <a href="http://www.ieee.org/portal/site/mainsite/menuitem.818c0c39e85ef176fb2275875bac26c8/index.jsp?&pName=corp_level1&path=pubs/transactions&file=stylesheets.xml&xsl=generic.xsl" target="_blank">IEEE Transaction style</a> for LaTeX and Word style files. Use the <STRONG>
-      <a href="http://www.ieee.org/organizations/pubs/confpub/auxfiles/sample_manuscript.pdf">sample manuscript</a></STRONG> as a visual aid for formatting and LaTeX example can be also found <a href="IEEE_CS_Latex.zip">here</a>.</P>
-<!--
-<p><b><font color="red">Camera Ready Format</font></b>: The camera ready format will be the same as the submission format, i.e., 4-5 or at most 6 pages long with a font size of 10pt. More information will be added after the paper acceptance date. </P>
--->
-<p><b><font color="red">Paper Submission</font></b>: </p>
-<p>See the <a href="authors.php">AUTHOR KIT</a> before submitting any papers to PFLDnet2010. </p>
-<!--
-<p>Authors should submit their papers through the EDAS web site <A href="http://www.edas.info/" target=_blank><strong>http://www.edas.info/</strong></A> in three steps: </p>
-<ol>
-<li>Creation of a personal account on EDAS (<B>if the author does not already have one)</B><br>
-(After the author completes log-in at http://www.edas.info/ follow the links to http://www.edas.info/listConferencesSubmit.php and scroll down the &quot;conference&quot; column on the webpage to PFLDnet2010, then follow the prompts to submit your paper.) </li>
-<li>Registration of the paper (requiring a short abstract of up to 150 words)</li>
-<li>Upload of the paper. <B>Only in pdf format</B></li>
-</ol>
-For help with EDAS please contact the <php email_link("edas-help AT edas info", "EDAS Administrator");>.
+<br><br>
+<p>The authors of accepted papers must guarantee that their paper will be presented at the conference. At least one author of each accepted paper must be registered for the conference in order for that paper to appear in the proceedings and to be scheduled for presentation. 
 </p>
--->
-<!--
-<p><b><font color="red">Rules</font></b>: Only original papers written in English that have not been published or submitted for publication elsewhere can be submitted.
-Full papers are required for review.
-All paper submissions will be carefully reviewed by at least two experts.
-Reviews will be returned to the author(s) with comments to ensure the high quality of the accepted papers.
--->
-<p>The authors of accepted papers must guarantee that their paper will be presented at the conference.
-At least one author of each accepted paper must be registered for the conference in order for that paper to appear in the proceedings and to be scheduled for presentation.
-</p>
+</div>
 </TD>
 </TR>
 </TABLE>
