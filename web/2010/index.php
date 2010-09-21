2700f3c226542a0434f95c84cf92f3663e77808a
Jana Iyengar <jiyengar@surutti.fandm.edu>
2010-09-21 18:46:42 -0400

    dates mod

--- web/2010/index.php
+++ web/2010/index.php
@@ -72,11 +72,11 @@ experiences in the area of transport layer problems.</p>
 <table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin-left:20px" class="detail">
   <tr>
     <td valign="top"><strong>Abstract submission:</strong></td>
-	 <td><span  style="text-decoration: line-through"><strong>September 20, 2010  October 4, 2010</strong></span><br/></td>
+	 <td><strong><span  style="text-decoration: line-through">September 20, 2010</span>  October 4, 2010</strong><br/></td>
   </tr>
   <tr>
     <td valign="top"><strong>Paper submission:</strong></td>
-    <td><span  style="text-decoration: none"><strong>September 27, 2010</strong><br/></span></td>
+	 <td><strong><span  style="text-decoration: none">September 27, 2010</span>  October 8, 2010</strong><br/></td>
   </tr>
   <tr>
     <td>Notification of acceptance:</td>
