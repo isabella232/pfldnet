785acac81a56ab676716d34c4facd29a40e990a9
Jana Iyengar <jiyengar@surutti.fandm.edu>
2010-10-28 12:39:09 -0400

    adding accomodation info

--- web/2010/accommodation.php
+++ web/2010/accommodation.php
@@ -1,31 +1,47 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
 <HTML lang="en">
+
 <HEAD>
 <META http-equiv="content-type" content="text/html;charset=ISO-8859-1">
-<TITLE>PFLDnet 2007 Accommodation</TITLE>
+<TITLE>PFLDNeT 2010 - Accomodation</TITLE>
 <LINK href="def.css" rel="stylesheet" media="all">
-<style type="text/css">
-<!--
-.style1 {color: #000000}
-.style3 {color: #FF0033}
--->
-</style>
 </HEAD>
 
-<?php $ROOT = "\\\\wil-fs\\wwwroot\\pfldnet2007"; ?>
-<?php include ($ROOT."\\templates\\header.php"); ?>
-<?php include("\\\\wil-fs\\wwwroot\\templates\\functions.html"); ?>
+<?php $ROOT = "."; ?>
+<?php include ("templates/header.php"); ?>
 
 <TABLE width="780" border="0" cellspacing="0" cellpadding="0">
 <TR>
 <TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="5" width="1" border="0"></TD>
-<TD width="778"><p style="text-align:right;float:right;margin-top:30px;margin-right:20px;font-size: 10pt; "><a href="regist.php">REGISTRATION</a> / <a href="visas.php">VISAS</a></p>
-<H1><IMG src="images/cap_acc.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">ACCOMMODATION</H1>
-  <div class="top">
+<TD width="778"><H1><IMG src="images/cap_sub.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">Accomodation</H1>
+<div class="top">
+	 <H2>Accomodation:</H2>
+<p>
+A block has been reserved for PFLDNeT attendees at 
+	 The Lancaster Mariott in downtown Lancaster.
+<b>Please use <a href="">this reservation link</a> 
+to make reservations in the PFLDNeT block</b>.
 
-    <H2>Accommodation  Reservations</H2>
-    <p>PFLDnet2007   delegates can select and book their accommodation from a list of various <a href="http://www.isi.edu/contact/accomodations.php"><strong>hotels</strong></a>  on the ISI USC,  website. </p>
-  <p><EM>VISA SUPPORT <span class="style1">CANNOT BEGIN <span class="style3">UNTIL</span></span> <a href="regist.php">REGISTRATION</a><span class="style1"> AND</span> <a href="http://www.isi.edu/contact/accomodations.php">HOTEL RESERVATIONS</a> <span class="style1">HAVE BEEN</span> COMPLETED.</EM> </p>
-  </div>
+<p>The Marriott is providing a group price of $139 per night, which includes wireless access.</p>
 
-<?php include ($ROOT."\\templates\\footer.php"); ?>
+<p>The <a href="http://www.marriott.com/hotels/travel/lnsmc-lancaster-marriott-at-penn-square/">Lancaster Mariott at Penn Square</a>
+is in downtown Lancaster (a National Historic District)
+which is a pleasant 25-minute walk (~1 mile/1.6 km) through town
+	from the workshop venue (F&M).
+Transportation will also be provided from the hotel
+in the morning, and to the hotel in the afternoon/evening.</p>
+</p>
+
+<p>
+	Other hotels in the area are listed <a href="http://fandm.edu/visit/dining-staying-in-lancaster#lodging">here</a>,
+	with the Lancaster Arts Hotel being the closest.
+</p>
+
+</div>
+</TD>
+</TR>
+</TABLE>
+
+
+<?php include ("templates/footer.php"); ?>
