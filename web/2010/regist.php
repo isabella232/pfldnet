0f42a9a84ac0573717d9820f1f7516899b3c9161
Jana Iyengar <jiyengar@surutti.fandm.edu>
2010-10-30 14:00:31 -0400

    updated regist page

--- web/2010/regist.php
+++ web/2010/regist.php
@@ -16,18 +16,22 @@
 <TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="5" width="1" border="0"></TD>
 <TD width="778"><H1><IMG src="images/cap_sub.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">Registration</H1>
 <div class="top">
+<H2>Registration Options</H2>
 <p>
 	 <b> Early Registration (Ends November 15, midnight EST):</b><br>
-	 General:  $200<br>
-	 Student:  $150
+	 General:  USD 200<br>
+	 Student:  USD 150
 </p>
 <p>
 	 <b> On-Site Registration:</b><br>
-	 General:  $250<br>
-	 Student:  $200
+	 General:  USD 250<br>
+	 Student:  USD 200
 </p>
 <p>
-	 Follow <a href="">this link</a> for early registration.
+	 <H2>Registration Steps</H2>
+	 <b>Step 1:</b> Pay your registration fee <a href="">here</a>.<br>
+	 <b>Step 2:</b> Fill out your personal information 
+	 and preferences <a href="">here</a>.
 </p>
 </div>
 </TD>
