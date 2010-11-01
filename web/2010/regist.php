8e05f3d66394133cfd5cc26df96892fa4a9d0472
Jana Iyengar <jiyengar@surutti.fandm.edu>
2010-10-31 23:47:36 -0400

    updated regist page

--- web/2010/regist.php
+++ web/2010/regist.php
@@ -16,6 +16,15 @@
 <TD bgcolor="#000000" width="1"><IMG src="images/spacer.gif" alt="" height="5" width="1" border="0"></TD>
 <TD width="778"><H1><IMG src="images/cap_sub.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">Registration</H1>
 <div class="top">
+<H2>Registration Steps</H2>
+<p><H2>
+	 Step 1: Pay registration fee 
+	 <a href="http://edas.info//r9523">here</a>.<br>
+	 Step 2: Fill out general information
+	 and preferences
+	 <a href="https://spreadsheets.google.com/viewform?formkey=dDFxejI2MVVDWURDNUxuZFA3Y3ZNRVE6MQ">here</a>.
+</H2></p>
+
 <H2>Registration Options</H2>
 <p>
 	 Regular Registration (Ends November 15, midnight EST):<br>
@@ -29,13 +38,6 @@
 	 An email confirming your registration will be sent to your 
 	 registered email address within two business days of payment.
 </p>
-<H2>Registration Steps</H2>
-	 <b>Step 1:</b> Pay registration fee 
-	 <a href="http://edas.info//r9523">here</a>.<br>
-	 <b>Step 2:</b> Fill out general information
-	 and preferences
-	 <a href="https://spreadsheets.google.com/viewform?formkey=dDFxejI2MVVDWURDNUxuZFA3Y3ZNRVE6MQ">here</a>.
-</p>
 
 <H2>Registration Policy</H2>
 <p>
@@ -48,7 +50,7 @@
 	 (such as wire-transfer charges).
 	 Refunds will not be made after November 15.
 </p>
-<p>
+
 <H2>Registration Inquiries</H2>
 <p>
 	 Jana Iyengar<br>
