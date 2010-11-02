d326d03286d3f0b5b1d91b9a900bc6e0d11e28cf
Jana Iyengar <jiyengar@surutti.fandm.edu>
2010-11-02 15:32:26 -0400

    syncing up

--- web/2010/access.php
+++ web/2010/access.php
@@ -30,22 +30,48 @@
 	 more detailed information on getting to the venue from 
      HIA and PHL follows.
 </p>
+     <H2>From Harrisburg Airport:</H2>
 <p>
-     <H2>From the Harrisburg Airport:</H2>
 	 <a href="">Harrisburg International Airport</a> is the nearest airport
 	 to the venue, 
-	 and is a 1-hour drive from the venue. 
-	 Shuttles can be 
-	 arranged to get to the venue 
+	 and is a ~45-minute drive from the venue.
+	 We have made arrangements with the following shuttle company:<br>
+	 Unique Limousine of Harrisburg:<br>
+	 Phone number: +1-800-298-8053<br>
+	 Town Car (upto 3 people): USD 116 (includes gratuity)<br><br>
+	 Van (4-10 people): USD 154 (includes gratuity)<br><br>
+	 Cancellation:  Allowed upto ~3 hours before pick-up time.<br><br>
+
+	 While you should budget for the worst case cost of USD 116 per person,
+	 we have arranged for sharing, and the best case cost is USD 15.4
+     when 10 people share a van. The shuttle company is co-ordinating 
+	 grouping and scheduling, and will try to accomodate sharing when possible.
+     When you call to make your reservation,
+	 please let them know the following: <br>
+	 (i) that you are with <b>the PFLDNeT group</b><br>
+	 (ii) that you are willing to share, and<br>
+	 (iii) how long you are willing to wait to share a shuttle.<br>
+
+	 You can call the phone number above closer to the travel date
+	 and check whether (and how much) you will be sharing.
+</p> 
+
+<!--	 <b>1.</b> Landis Luxury Coaches (Call a week in advance)<br>
+	 Phone numbers: +1-800-325-9004, +1-717-859-5466<br>
+	 HIA -> Lancaster:  USD 108.10 (per person, including 15% gratuity)<br>
+	 If you co-ordinate with other attendees, you can avail of better
+	 prices per person:<br>
+	 booking for a two-person group: USD 161.00 (including 15% gratuity)<br>
+	 booking for 3-5-person group: USD 54.05 per person (including 15% gratuity)
+<br><br>
      (see <a href="http://www.flyhia.com/home/parking/limos">here</a>
 	  for a list), and 
 	 <a href="http://www.flyhia.com/home/parking/car_rental">rentals</a> 
 	 are available at the airport as well;
 	 more information will be posted here shortly.<br>
-    (We are also working on making alternative cheaper options available.)
-</p>
+-->	 
 <p>
-	 <H2>From the Philadelphia Airport:</H2>
+	 <H2>From Philadelphia Airport:</H2>
 	 <a href="http://www.phl.org">Philadelphia International Airport</a>
 	 is a 1.5-hour drive from the venue,
 	 and may be preferred for those planning to continue on
@@ -58,18 +84,22 @@
 	 Tickets can be purchased 
 	 at the train station. (Travel time: ~30 minutes)<br>
 	 <b>2.</b> Use <a href="http://www.amtrak.com">Amtrak</a>
-	 to get from 30th Street Station (station code: PHL) 
-		 to Lancaster (station code: LNC). Tickets can be purchased at 
-		 kiosks or windows 
-		 at 30th Street Station, or <a href="http://www.amtrak.com">online</a>. 
-	        (Travel time: ~70 minutes)<br>
+	 to get from "30th Street" Station (station code: PHL) 
+	 to Lancaster (station code: LNC). Tickets can be purchased at 
+	 kiosks or windows 
+	 at 30th Street Station, or <a href="http://www.amtrak.com">online</a>. 
+	 If you purchase your tickets online, and if you miss your train, go
+	 to the customer service desk at 30th Street Station and pay the 
+     difference (if any) for the next train.  In case the customer service 
+     desk is closed, assume that the ticket will work on the following train.
+	 (Travel time: ~70 minutes)<br>
 	 <b>3.</b>The Mariott is a ~25-minute walk 
 		 (<a href="http://maps.google.com/maps?f=d&source=embed&saddr=Lancaster+Amtrak,+Lancaster,+PA&daddr=Lancaster+Marriott+at+Penn+Square,+Lancaster,+PA&geocode=FaUtYwId_qJz-ynbbjkahSTGiTEV_D_wL2Wj4g%3BFcXqYgIdsKtz-yEdOStVjs5O_infEI6P7STGiTG4n8MR0H61Fw&hl=en&mra=ltm&dirflg=w&sll=40.045725,-76.3081&sspn=0.023096,0.051627&ie=UTF8&ll=40.045752,-76.307988&spn=0.023096,0.051627&z=15">walking directions</a>)
 		 or a 5-minute taxicab ride
 		 from the Lancaster Amtrak train station.<br>
 </p>
 <p>
-		 <H2>Taxicab Information:</H2>
+		<H2>Taxicab Information (in no particular order):</H2>
 		 <b>1.</b> Friendly Transportation: 1-800-795-FAST<br>
 		 <b>2.</b> Yellow Cab: 717-397-8100
 </p>
