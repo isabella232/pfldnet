ecf981af3fb214a0305e45a49cec6b677e7388ef
Jana Iyengar <jiyengar@surutti.fandm.edu>
2010-11-18 15:01:00 -0500

    added amtrak notice

--- web/2010/access.php
+++ web/2010/access.php
@@ -17,12 +17,12 @@
 <TD width="778"><H1><IMG src="images/cap_sub.gif" alt=" " height="36" width="20" align="absmiddle" border="0" hspace="5" vspace="5">Getting There</H1>
 <div class="top">
 <p>
-	 <H2> Special Amtrak Notice: </H2>
+	 <H2> Special Amtrak Notice:
 	 Since it is Thanksgiving season
 	 and the busiest travel season of the year,
 	 Amtrak requires <b>advance</b> reservations
 	 for any train from November 23 through 29.  Please make your
-	 reservations accordingly.
+	 reservations accordingly.</H2>
 </p>
 <p>
 	 <H2> General Instructions: </H2>
